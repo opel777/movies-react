7866eaf55983e5f9ca51caef05e80c3d5d09ed7a
Gustavo <villegasgustavoopel@hotmail.com>
2022-05-15 17:32:09 -0300

    useeffect a search movie

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,36 @@
 import style from './Search.module.css';
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch } from 'react-icons/fa';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useQuery } from '../hooks/useQuery';
+import { useEffect } from 'react';
 
 export function Search() {
+    const query = useQuery();
+    const search = query.get("search");
+
+    const [searchText, setSearchText] = useState("");
+    const history = useHistory();
+
+    useEffect(() => {
+        setSearchText(search || "");
+    }, [search]);
+
+    const handleSubmit = (e)=> {
+      e.preventDefault();
+      history.push("/?search=" + searchText)
+    };
     return (
-        <form className={style.searchContainer}>
+        <form className={style.searchContainer} onSubmit={handleSubmit}>
             <div className={style.searchBox}>
-                <input className={style.searchInput} type= "text" />
+                <input
+                 className={style.searchInput} 
+                 type= "text" 
+                 value={searchText}
+                 onChange={(e) => setSearchText(e.target.value)} 
+                />
                 <button className={style.searchButton} type= "submit">
-                 <FaSearch  size = {20}/>
+                 <FaSearch  size = {20} />
                 </button>
             </div>
         </form>
